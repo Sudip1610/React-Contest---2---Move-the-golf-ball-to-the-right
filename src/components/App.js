cae96e1c5a394d96804f44cd38376639584c2764
Sudip Ghosh <ghoshsudip084@gmail.com>
2020-10-25 20:46:18 +0530

    Change 2

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     super(props);
     this.state = {
       renderBall: false, //variable needed to be changed
-      posi: 0,
+      posi: 5,
       ballPosition: { left: "0px" }
     };
     this.renderChoice = this.renderChoice.bind(this);
